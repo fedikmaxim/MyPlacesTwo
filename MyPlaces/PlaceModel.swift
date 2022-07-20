5729ed09be2828704dd65284a461992abaa502ec
Maxim <fedykmaxim@gmail.com>
2022-07-20 17:23:10 +0300

    Places Array

---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,25 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Maxim on 20.07.2022.
+//  Copyright © 2022 Alexey Efimov. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+}
